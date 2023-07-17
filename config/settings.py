4834195fb4123aadfef43d4f4245b1d169aadf13
Seyi Pythonian <prontomaster@gmail.com>
2023-07-17 04:45:43 +0100

    added currency filter

--- config/settings.py
+++ config/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django.contrib.flatpages",
     "django.contrib.sites",
+    "django.contrib.humanize",
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
